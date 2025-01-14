d52f0a847a615892f444272feed6abf7a3df30e0
Firza Irvan <94727919+firzzairvn@users.noreply.github.com>
2025-01-15 05:20:34 +0700

    build : update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <>
       <div>
-        <p className="h-12">Copyright © 2023</p>
+        <p className="h-12">Copyright © 2025 - All right reserved</p>
       </div>
     </>
   );
